2f71aa8d35a828191d17a5ff85a37e409d65de20
Bennexy <74596167+Bennexy@users.noreply.github.com>
2020-11-19 23:45:59 +0100

    19.11.2020
    Backup und Logging Modul Funktionieren. habe noch ein paar anpassungen zum spielbegin gemacht. Verhindert das zweifache erstellen eines Charakters.

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
-import random
 import map
 import characters
 from configparser import ConfigParser
 import time
-from pathlib import Path
 import os
 import sys
+import regex as re
+from Backup_py import Logger
+from Backup_py import Backup as Backup
 
+Logger = Logger()
 cfg = ConfigParser()
 # Ist jetzt eine sehr unscchöne lösung die aber recht gut funktionieren sollte
 global interim
 interim = 0
+print(os.getcwd())
+
+
+# save location
+def saved_file_loc(name=characters.PLAYER[characters.NAME]):
+    if name == "" or name is None:
+        name = characters.PLAYER[characters.NAME]
+    else:
+        pass
+    save_file_name = os.path.join("Saved_Games", "Config_TA_" + name + ".ini")
+    Logger.all_log("Name of folder and save " + save_file_name + Logger.lineno())
+    return save_file_name
 
 
 def game_help():
-    print(commands.keys())
+    key_value = ""
+    for i in commands.keys():
+        key_value = key_value + " -" + i + "- "
+    print(key_value)
+    eingabe()
 
 
 # load saved game
-def load():
+def load(extra=False):
     namedata = input("Please enter the name of the saved character: ")
-    data = "Config_TA_" + namedata + ".txt"
+    data = saved_file_loc(namedata)
     cfg.read(data)
 
     # check ob die file exsistiert
@@ -30,11 +48,16 @@ def load():
     elif os.path.isfile(data) is False:
         # beendet den load vorgang und startet die eingabe erneut
         print("Dieser Speicherstand ist nicht vorhanden")
-        # Lösung Problem Nr. 0
-        eingabe()
+        if extra:
+            game_load()
+            return
+        else:
+            # Lösung Problem Nr. 0
+            eingabe()
+            return
     else:
         # diesen teil darf es auf keinen fall erreichen sonst ist bool weder True noch False
-        # log wird noch eingebaut
+        Logger.all_log("Fehler 0 in main.py" + Logger.lineno())
         sys.exit("Crit Problem in load siehe log")
 
     # Playerdaten
@@ -54,14 +77,28 @@ def load():
 
 
 # save pending game
-def save():
-    # noch ohne backup
+def save(extra=None):
     global interim
-    # von Datei time rauslesen wenn sie exestiert
-    # Frage: ist das die Spieldauer Zeit oder welche Zeit ist dies?
-    # Frage wenn Datei schon vorhanden, sollen wir dann fragen ob diese überschrieben werden soll?
-    # oder gleich ein backup erstellen und Frage lassen? - dann würde ich kein backup beim laden des spieles machen
 
+    # von Datei time rauslesen wenn sie existiert
+    # Überprüfung ob der Speicherstan vorhanden ist
+
+    save_file = saved_file_loc(characters.PLAYER[characters.NAME])
+    if os.path.isfile(save_file):
+        ueberschrieben = input("Spielstand Überschreiben? (J/N): ")
+
+        if ueberschrieben == "J" or ueberschrieben == "j":
+            Backup(save_file)
+        elif ueberschrieben == "N" or ueberschrieben == "n":
+            print("Saving canceled")
+            eingabe()
+        else:
+            print("Unknown Input")
+            save()
+    else:
+        Logger.all_log("Dir muss erst erstellt werden" + Logger.lineno())
+
+    print("Game is Saving")
     tosavetime = interim + characters.savetime
 
     cfg["config_TA"] = {"Player": characters.PLAYER[characters.NAME],
@@ -73,12 +110,23 @@ def save():
     cfg["config_Skill"] = {"Attacklevel": characters.fightlevel,
                            "AttackEP": characters.PlayerFightEp}  # Alles zu Skills
 
-    with open("Config_TA_" + characters.PLAYER[characters.NAME] + ".txt", "w") as file:
+    with open(saved_file_loc(), "w") as file:
         cfg.write(file)
+
+    Logger.all_log("Pending game saved" + Logger.lineno())
+
+    if extra == "end":
+        sys.exit("Byby")
+    elif extra is None:
+        eingabe()
+    else:
+        Logger.all_log("passed argument to save is not known")
+        pass
 
 
 def player_get_money():
     print("You have " + str(characters.PLAYER[characters.COINS]) + " coins")
+    eingabe()
 
 
 def player_info():
@@ -94,6 +142,7 @@ def player_info():
           "\n\tHealth: " + str(characters.PLAYER[characters.HEALTH]),
           "\n\tFight Ep: " + str(characters.PlayerFightEp),
           "\n\n\n")
+    eingabe()
 
 
 def run():
@@ -103,6 +152,7 @@ def run():
 commands = {
     "help": game_help,
     "quit": lambda: exit("You commit suicide and leave this world."),
+    "end": lambda: exit(save("end")),
     "rest": characters.game_rest_player,
     "forward": map.forward,
     "backwards": map.backwards,
@@ -129,31 +179,67 @@ def eingabe():
 
     start = time.time()
     command = input("> ").lower().split(" ")[0]
-    # Problem Nr. 0
-    # hier wird der Spieler automatisch auch nach jeder unrelevanten eingabe auch load save etc. in die nächste aktion
-    # gesetzt ist das gewollt?
+    # Übermittelt die eeingabe und den Namen des characters
+    Logger.eingabe_log(command, characters.PLAYER[characters.NAME])
+
     if command in commands:
         commands[command]()
         if characters.rest_counter == 1 or characters.rest_counter == 0:
             characters.rest_available = True
         elif command in ("left", "right", "forward", "backwards"):
+            # verhindert das spammen von rest
             characters.rest_counter -= 1
 
     elif not characters.rest_available:
         print(
-            "You run around in circles and don't know what to do\nKeep moving to rest")
+            "You run around in circles and don't know what to do\nKeep moving to rest"
+        )
     else:
-        print(
-            "You run around in circles and don't know what to do")
+        print("This command doesn't exsist")
+        eingabe()
+
     map.print_current_enemies()
     interim = interim + (time.time() - start)
 
 
+def game_load():
+    first = input("new game or load game? : ")
+    new_game = ["new", "New", "new game", "New game", "New Game"]
+    load_game = ["load", "Load", "load game", "Load game", "Load Game"]
+    if first in new_game:
+        buffer = input("Enter your name: ")
+        # liste mit nicht gültigen namen - exsistieren schon oder ist leer -> ""
+        # #dies können wir auch beleibig erweitern
+        buffer_not = re.findall(r"(?<=Config_TA_)([A-z]+\s?)*(?=\.ini)", str(os.listdir("Saved_Games")))
+        # hier können wir Bestimmte eingaben verhindern
+        buffer_not_valid = ["", " ", "Bennexy"]
+        if buffer not in buffer_not and buffer not in buffer_not_valid:
+            characters.PLAYER[characters.NAME] = buffer
+        elif buffer in buffer_not:
+            print("Name is allready taken")
+            game_load()
+        elif buffer in buffer_not_valid:
+            print("Name is not Valid")
+            game_load()
+        else:
+            Logger.log("Fehler 0 in main.py" + Logger.lineno())
+            sys.exit("Crit Problem in game_load siehe log")
+
+        print("Player Charakter " + characters.PLAYER[characters.NAME] + " made")
+        pass
+    elif first in load_game:
+        load(True)
+        pass
+    else:
+        print("Command not found")
+        game_load()
+
+
 if __name__ == "__main__":
 
-    characters.PLAYER[characters.NAME] = input("Enter your name: ")
+    game_load()
+    # print(type(characters.PLAYER[characters.NAME]), characters.PLAYER[characters.NAME])
     map.init(10, 10)
     print("Type -help- to list the available commands\n")
     while True:
         eingabe()
-
